d2bc7c2ebcd6ba02a732d973f271e501c46ba4b7
vanxh <hello@vanxh.dev>
2024-07-11 17:04:00 +0400

    make blockchain data use ethers v6

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,8 +1,8 @@
-export * as config from './config';
-export * as blockchainData from './blockchain_data';
-export * as passport from './passport';
-export * as orderbook from './orderbook';
+export * as config from './config'; // used // done
+export * as blockchainData from './blockchain_data'; // used // in progress
+export * as passport from './passport'; // used // todo
+export * as orderbook from './orderbook'; // used // todo
 export * as checkout from './checkout';
-export * as x from './x';
+export * as x from './x'; // used // todo
 export * as webhook from './webhook';
 export * as mintingBackend from './minting_backend';
